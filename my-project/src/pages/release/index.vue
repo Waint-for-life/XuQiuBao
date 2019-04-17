6185b82fda00ad2a3b849cf976fb672f48268897
student <student@hubu.edu.cn>
2019-04-17 10:44:14 +0800

    忽略了.idea，设计了发布页的滑轮

--- my-project/src/pages/release/index.vue
+++ my-project/src/pages/release/index.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
     <view i-class="release">
-<i-panel title="发布需求" i-class="release">
+<i-panel title="发布需求(滑动选择需求种类:)" i-class="release">
     <i-input i-class="release" type="textarea" placeholder="请输入你的需求内容(最多150字)" maxlength="150" />
 </i-panel>
+   <view>
+  <picker-view indicator-style="height: 50px;"  style="width: 100%; height: 300px;" :value="value" @change="bindChange">
+    <picker-view-column>
+      <view >{{item}}年</view>
+    </picker-view-column>
+    <picker-view-column>
+      <view  >{{item}}月</view>
+    </picker-view-column>
+    <picker-view-column>
+      <view >{{item}}日</view>
+    </picker-view-column>
+  </picker-view>
+</view>
   <i-button type="primary" >发布</i-button>
     </view>
   </div>
 </template>
-
 <script>
   export default {
     name: 'index.vue',
